edc2a07e65d1554dad0206981a44bca058da2170
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2022-09-17 22:43:21 -0300

    projeto basico de conta trilha java basico DIO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        //TODO: Conhecer e importar a classe Scanner
+        //TODO: Exibir as mensagens para o nosso usuário 
+        //TODO: Obter pela scanner os valores digitados no terminal
+        //TODO: Exibir a mensagem conta criada
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scan.nextLine();
+        System.out.println();
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int numero = scan.nextInt();
+        System.out.println();
+
+        System.out.println("Por favor, digite o nome do Cliente!");
+        scan.nextLine();
+        String nomeCliente = scan.nextLine();
+        System.out.println();
+
+        System.out.println("Por favor, digite o saldo da conta!");
+        System.out.println();
+        double saldo = scan.nextDouble();
+        System.out.println();
+        
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
+
+        scan.close();
     }
 }
